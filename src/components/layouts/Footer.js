8959a736f82062795491b6f7127bf315ddd62a11
Ashish Babu Rao <110420433+ashish123256@users.noreply.github.com>
2023-12-18 11:27:04 +0530

    Update Footer.js

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
         <p className="m-0">
           All copyrights reserved, developed by{" "}
           <a href="/about" style={{ textDecoration: "none" }}>
-            <b>Ashish Babu Rao </b>
+            <b>Ashish Rao </b>
           </a>
           2023
         </p>
